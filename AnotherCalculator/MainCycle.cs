b3a4a526d2a1e3db142f8d70d5d72b163d09c086
WhiteKoldun <50768001+WhiteKoldun@users.noreply.github.com>
2021-06-13 19:40:32 +0300

    проверки на формат, нормализация

--- AnotherCalculator/MainCycle.cs
+++ AnotherCalculator/MainCycle.cs
@@ -6,9 +6,12 @@ namespace AnotherCalculator
 {
     class MainCycle
     {
+        bool isProgramWorkin;
+        string forbiddenChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,./?;:'\"\\|~`_=!@#$%%^&*";
+
         public void Cycle()
         {
-            for (bool isProgramWorkin = true; isProgramWorkin;)
+            for (isProgramWorkin = true; isProgramWorkin;)
             {
                 Console.WriteLine("Введите новое выражение, введите \"q\" для завершения:");
                 var rawInput = Console.ReadLine();
@@ -16,7 +19,31 @@ namespace AnotherCalculator
                 {
                     isProgramWorkin = false;
                 }
+                int result = new int();
+                CheckFormatOfInput(rawInput);
+                rawInput = NormaliseRawInput(rawInput);
+                Console.WriteLine("Результат = " + result + "debug: " +rawInput);
             }
+        }
+
+        private void CheckFormatOfInput(string rawInput)
+        {
+            rawInput.ToUpper();
+            char[] forbiddenCharsArray = forbiddenChars.ToCharArray();
+            int checkResult = rawInput.LastIndexOfAny(forbiddenCharsArray);
+            if (checkResult != -1)
+            {
+                isProgramWorkin = false;
+            }
+
+
+        }
+
+        private string NormaliseRawInput(string rawInput)
+        {
+            rawInput = rawInput.Replace(" ", "");
+            var rawInputNormalised = rawInput.ToUpper();
+            return rawInputNormalised;
         }
     }
 }
