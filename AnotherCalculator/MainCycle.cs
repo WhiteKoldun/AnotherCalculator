a4975278671d734676c271f979a49fee26f4b225
WhiteKoldun <50768001+WhiteKoldun@users.noreply.github.com>
2021-06-13 19:07:46 +0300

    Стартуем

---
+++ AnotherCalculator/MainCycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnotherCalculator
+{
+    class MainCycle
+    {
+        public void Cycle()
+        {
+            for (bool isProgramWorkin = true; isProgramWorkin;)
+            {
+                Console.WriteLine("Введите новое выражение, введите \"q\" для завершения:");
+                var rawInput = Console.ReadLine();
+                if (rawInput.ToUpper() == "Q")
+                {
+                    isProgramWorkin = false;
+                }
+            }
+        }
+    }
+}
---
+++ AnotherCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnotherCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствую в приложении \"Очередной калькулятор\"! Составляйте выражения из целых чисел, знаков сложения и вычитания и скобок!");
+            MainCycle mainCycle = new MainCycle();
+            mainCycle.Cycle();
+        }
+
+        
+    }
+}
